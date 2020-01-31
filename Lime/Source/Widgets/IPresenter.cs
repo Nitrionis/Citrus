12fe6d1a503c1909c8628f7cc7968740dd86f2ab
Nitrionis <nitrodimon@mail.ru>
2020-01-31 16:41:18 +1000

    [Lime] Connecting PlatformProfiler to engine. Phase four done.

--- Lime/Source/Widgets/IPresenter.cs
+++ Lime/Source/Widgets/IPresenter.cs
@@ -46,6 +46,9 @@ namespace Lime
 			for (var i = Count - 1; i >= 0; i--) {
 				var obj = this[i].GetRenderObject(node);
 				if (obj != null) {
+#if PROFILER_GPU
+					ro.SetProfilerData(node, node.Manager);
+#endif
 					ro.Objects.Add(obj);
 				}
 			}
