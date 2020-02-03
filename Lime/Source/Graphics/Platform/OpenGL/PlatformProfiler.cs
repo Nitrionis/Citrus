e0bb75a9389cfcda6f798889b79ba198c095d3c8
Nitrionis <nitrodimon@mail.ru>
2020-02-03 11:26:31 +1000

    [Lime] Add PlatformProfiler implementations for Vulkan and OpenGL.

---
+++ Lime/Source/Graphics/Platform/OpenGL/PlatformProfiler.cs
@@ -0,0 +1,35 @@
+
+namespace Lime.Graphics.Platform.OpenGL
+{
+	/// <summary>
+	/// PlatformProfiler implementation for OpenGL.
+	/// </summary>
+	/// <remarks>
+	/// Unable to get render call detail for OpenGL ES 2.0.
+	/// </remarks>
+	internal class PlatformProfiler : Platform.PlatformProfiler
+	{
+		public override bool IsDeepProfiling { get => false; set { } }
+
+		public void DrawCall(ProfilingInfo profilingInfo, int vertexCount, PrimitiveTopology topology)
+		{
+			if (isProfilingEnabled) {
+				resultsBuffer.FullVerticesCount += vertexCount;
+				int trianglesCount = CalculateTrianglesCount(vertexCount, topology);
+				resultsBuffer.FullTrianglesCount += trianglesCount;
+				if (profilingInfo.IsPartOfScene) {
+					resultsBuffer.SceneDrawCallCount++;
+					resultsBuffer.SceneVerticesCount += vertexCount;
+					resultsBuffer.SceneTrianglesCount += trianglesCount;
+				}
+				resultsBuffer.FullDrawCallCount++;
+			}
+		}
+
+		internal override void FrameRenderFinished()
+		{
+			resultsBuffer.IsDeepProfilingEnabled = false;
+			base.FrameRenderFinished();
+		}
+	}
+}
