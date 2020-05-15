5abee69c57e38707c9b2a195f6e0c2e2144bf833
Nitrionis <35944960+Nitrionis@users.noreply.github.com>
2020-05-15 16:08:21 +1000

    [Profiler][Lime] Add LimeProfiler.

---
+++ Lime/Source/Profilers/LimeProfiler.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Threading;
+using Lime.Profilers.Contexts;
+using ProfilerContext = Lime.Profilers.Contexts.Context;
+using GpuHistory = Lime.Graphics.Platform.Profiling.GpuHistory;
+using GpuProfiler = Lime.Graphics.Platform.Profiling.RenderGpuProfiler;
+
+namespace Lime.Profilers
+{
+	public class LimeProfiler
+	{
+		private static LimeProfiler instance;
+
+		public static void Initialize()
+		{
+			if (instance == null) {
+				instance = new LimeProfiler();
+			}
+		}
+
+		private static ProfilerContext nextContext;
+		public static ProfilerContext CurrentContext { get; private set; }
+
+		public static Action ContextChanged;
+		public static Action LocalDeviceUpdateStarted;
+		public static Action LocalDeviceFrameRenderCompleted;
+
+		public static GpuHistory GpuHistory { get => CurrentContext.GpuHistory; }
+		public static CpuHistory CpuHistory { get => CurrentContext.CpuHistory; }
+
+		public static bool IsProfilingEnabled
+		{
+			get => CurrentContext.IsProfilingEnabled;
+			set => CurrentContext.IsProfilingEnabled = value;
+		}
+
+		public static bool IsDrawCallsRenderTimeEnabled
+		{
+			get => CurrentContext.IsDrawCallsRenderTimeEnabled;
+			set => CurrentContext.IsDrawCallsRenderTimeEnabled = value;
+		}
+
+		public static bool IsSceneOnlyDrawCallsRenderTime
+		{
+			get => CurrentContext.IsSceneOnlyDrawCallsRenderTime;
+			set => CurrentContext.IsSceneOnlyDrawCallsRenderTime = value;
+		}
+
+		private LimeProfiler()
+		{
+			CurrentContext = new LocalContext();
+			SetContext(CurrentContext);
+			CpuProfiler.Instance.Updating = OnLocalDeviceUpdateStarted;
+			GpuProfiler.Instance.FrameRenderCompleted += OnLocalDeviceFrameRenderCompleted;
+		}
+
+		public void OnLocalDeviceUpdateStarted()
+		{
+			if (nextContext != null) {
+				CurrentContext?.Completed();
+				CurrentContext = Interlocked.Exchange(ref nextContext, null);
+				CurrentContext.Activated();
+				ContextChanged?.Invoke();
+			}
+			CurrentContext.LocalDeviceUpdateStarted();
+			LocalDeviceUpdateStarted?.Invoke();
+		}
+
+		public void OnLocalDeviceFrameRenderCompleted()
+		{
+			CurrentContext.LocalDeviceFrameRenderCompleted();
+			LocalDeviceFrameRenderCompleted?.Invoke();
+		}
+
+		public static void SetContext(ProfilerContext context) => nextContext = context;
+	}
+}
