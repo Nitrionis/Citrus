6a41ee894da02edab0e7dd7423264732c59eb8f8
Nitrionis <35944960+Nitrionis@users.noreply.github.com>
2020-05-15 19:40:16 +1000

    [Profiler][Tangerine] Add profiler colors to theme editor.

--- Tangerine/Tangerine.UI/Utils/Colors.cs
+++ Tangerine/Tangerine.UI/Utils/Colors.cs
@@ -271,6 +271,72 @@ namespace Tangerine.UI
 			public Color4 JointColor { get; set; }
 		}
 
+		public class ProfilerColors : Theme.DefaultColors
+		{
+			[YuzuOptional]
+			public Color4 Background { get; set; }
+			[YuzuOptional]
+			public Color4 LegendText { get; set; }
+			[YuzuOptional]
+			public Color4 ChartsBackground { get; set; }
+			[YuzuOptional]
+			public Color4 ChartOne { get; set; }
+			[YuzuOptional]
+			public Color4 ChartTwo { get; set; }
+			[YuzuOptional]
+			public Color4 ChartThree { get; set; }
+			[YuzuOptional]
+			public Color4 ChartFour { get; set; }
+			[YuzuOptional]
+			public Color4 ChartFive { get; set; }
+			[YuzuOptional]
+			public Color4 ChartSix { get; set; }
+			[YuzuOptional]
+			public Color4 ChartSeven { get; set; }
+			[YuzuOptional]
+			public Color4 ChartEight { get; set; }
+			[YuzuOptional]
+			public Color4 ChartNine { get; set; }
+			[YuzuOptional]
+			public Color4 TimelineBackground { get; set; }
+			[YuzuOptional]
+			public Color4 TimelineHeaderBackground { get; set; }
+			[YuzuOptional]
+			public Color4 TimelineRulerAndText { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallUnknownOne { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallUnknownTwo { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallUiOne { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallUiTwo { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallSceneOne { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallSceneTwo { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallUnselectedOne { get; set; }
+			[YuzuOptional]
+			public Color4 DrawCallUnselectedTwo { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageUnselected { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageAnimation { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageUpdate { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageGesture { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageRenderPreparation { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageNodeRender { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageBatchRender { get; set; }
+			[YuzuOptional]
+			public Color4 CpuUsageOwnerUnknown { get; set; }
+		}
+
 		[YuzuOptional]
 		public bool IsDark { get; set; }
 		[YuzuOptional]
@@ -295,6 +361,8 @@ namespace Tangerine.UI
 		public KeyboardColors Keyboard { get; set; }
 		[YuzuOptional]
 		public HierarchyColors Hierarchy { get; set; }
+		[YuzuOptional]
+		public ProfilerColors Profiler { get; set; }
 
 		public ColorTheme Clone()
 		{
@@ -448,6 +516,36 @@ namespace Tangerine.UI
 				MatchColor = Color4.Yellow.Darken(0.5f).Transparentify(0.6f),
 				JointColor = Color4.Gray,
 			};
+			var profiler = new ProfilerColors {
+				Background = new Color4(40, 44, 52),
+				ChartsBackground = new Color4(33, 37, 43),
+				LegendText = new Color4(204, 204, 204),
+				ChartOne = new Color4(76, 176, 80),
+				ChartTwo = new Color4(33, 150, 243),
+				ChartThree = new Color4(254, 87, 34),
+				ChartFour = new Color4(255, 235, 60),
+				ChartFive = new Color4(0, 150, 136),
+				ChartSix = new Color4(165, 208, 97),
+				TimelineBackground = new Color4(33, 37, 43),
+				TimelineHeaderBackground = new Color4(33, 37, 43),
+				TimelineRulerAndText = new Color4(204, 204, 204),
+				DrawCallUnknownOne = new Color4(244, 67, 54),
+				DrawCallUnknownTwo = new Color4(254, 152, 0),
+				DrawCallSceneOne = new Color4(0, 150, 136),
+				DrawCallSceneTwo = new Color4(76, 176, 80),
+				DrawCallUiOne = new Color4(63, 81, 181),
+				DrawCallUiTwo = new Color4(33, 150, 243),
+				DrawCallUnselectedOne = new Color4(60, 60, 60),
+				DrawCallUnselectedTwo = new Color4(60, 60, 60),
+				CpuUsageUnselected = new Color4(60, 60, 60),
+				CpuUsageAnimation = new Color4(103, 58, 183),
+				CpuUsageUpdate = new Color4(33, 150, 243),
+				CpuUsageGesture = new Color4(255, 193, 7),
+				CpuUsageRenderPreparation = new Color4(0, 150, 136),
+				CpuUsageNodeRender = new Color4(76, 175, 80),
+				CpuUsageBatchRender = new Color4(177, 220, 57),
+				CpuUsageOwnerUnknown = new Color4(244, 67, 54)
+			};
 			return new ColorTheme {
 				IsDark = true,
 				Toolbar = toolbar,
@@ -461,6 +559,7 @@ namespace Tangerine.UI
 				Inspector = inspector,
 				Keyboard = keyboard,
 				Hierarchy = hierarchy,
+				Profiler = profiler,
 			};
 		}
 
@@ -602,6 +701,36 @@ namespace Tangerine.UI
 				MatchColor = Color4.Yellow.Transparentify(0.6f),
 				JointColor = Color4.Gray,
 			};
+			var profiler = new ProfilerColors {
+				Background = new Color4(40, 44, 52),
+				ChartsBackground = new Color4(33, 37, 43),
+				LegendText = new Color4(204, 204, 204),
+				ChartOne = new Color4(76, 176, 80),
+				ChartTwo = new Color4(33, 150, 243),
+				ChartThree = new Color4(254, 87, 34),
+				ChartFour = new Color4(255, 235, 60),
+				ChartFive = new Color4(0, 150, 136),
+				ChartSix = new Color4(165, 208, 97),
+				TimelineBackground = new Color4(33, 37, 43),
+				TimelineHeaderBackground = new Color4(33, 37, 43),
+				TimelineRulerAndText = new Color4(204, 204, 204),
+				DrawCallUnknownOne = new Color4(244, 67, 54),
+				DrawCallUnknownTwo = new Color4(254, 152, 0),
+				DrawCallSceneOne = new Color4(0, 150, 136),
+				DrawCallSceneTwo = new Color4(76, 176, 80),
+				DrawCallUiOne = new Color4(63, 81, 181),
+				DrawCallUiTwo = new Color4(33, 150, 243),
+				DrawCallUnselectedOne = new Color4(60, 60, 60),
+				DrawCallUnselectedTwo = new Color4(60, 60, 60),
+				CpuUsageUnselected = new Color4(60, 60, 60),
+				CpuUsageAnimation = new Color4(103, 58, 183),
+				CpuUsageUpdate = new Color4(33, 150, 243),
+				CpuUsageGesture = new Color4(255, 193, 7),
+				CpuUsageRenderPreparation = new Color4(0, 150, 136),
+				CpuUsageNodeRender = new Color4(76, 175, 80),
+				CpuUsageBatchRender = new Color4(177, 220, 57),
+				CpuUsageOwnerUnknown = new Color4(244, 67, 54)
+			};
 			return new ColorTheme {
 				IsDark = false,
 				Toolbar = toolbar,
@@ -615,6 +744,7 @@ namespace Tangerine.UI
 				Inspector = inspector,
 				Keyboard = keyboard,
 				Hierarchy = hierarchy,
+				Profiler = profiler
 			};
 		}
 	}
