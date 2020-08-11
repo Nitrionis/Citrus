187aa611d954117ee2efc93f1af22ba29fe4ed45
Dmitry Shcherbina <35944960+Nitrionis@users.noreply.github.com>
2020-08-11 20:55:20 +1000

    [CIT-844][Overdraw][Profiler][Lime][Win][Android][Mac][iOS] Add RenderBatchOwnersInfo struct.

---
+++ Lime/Source/Profiler/Graphics/RenderBatchOwnersInfo.cs
@@ -0,0 +1,19 @@
+#if PROFILER || OVERDRAW
+namespace Lime.Profiler.Graphics
+{
+	public struct RenderBatchOwnersInfo
+	{
+		public bool IsPartOfScene { get; private set; }
+
+		public void Initialize()
+		{
+			IsPartOfScene = false;
+		}
+
+		public void ProcessNode(IProfileableObject node)
+		{
+			IsPartOfScene |= node == null || node.IsPartOfScene;
+		}
+	}
+}
+#endif // PROFILER || OVERDRAW
