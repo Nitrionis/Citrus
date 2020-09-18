53fb73ea22cafae3bfa5891a4683a329cc59df79
Dmitry Shcherbina <35944960+Nitrionis@users.noreply.github.com>
2020-09-18 17:21:34 +1000

    [ProfilerCharts][Tangerine][Win][Mac] Add FixedSpacingChartsExtension.

---
+++ Tangerine/Tangerine.UI.Profiler/Charts/FixedSpacingChartsExtension.cs
@@ -0,0 +1,47 @@
+using System;
+using static Tangerine.UI.Charts.FixedHorizontalSpacingCharts;
+
+namespace Tangerine.UI.Charts
+{
+	internal static class FixedSpacingChartsExtension
+	{
+		/// <summary>
+		/// The original values will be shifted 1 to the left and inserts point at the end of the array.
+		/// </summary>
+		public static void Enqueue(this IFixedSpacingChart chart, float point)
+		{
+			var heights = chart.Heights;
+			Array.Copy(heights, 1, heights, 0, heights.Length - 1);
+			heights[heights.Length - 1] = point;
+		}
+
+		/// <summary>
+		/// Gets the maximum height among the chart points. O(n).
+		/// </summary>
+		public static float MaxValue(this IFixedSpacingChart chart)
+		{
+			float value = float.NegativeInfinity;
+			var heights = chart.Heights;
+			unchecked {
+				for (int i = 0; i < heights.Length; i++) {
+					value = Math.Max(value, heights[i]);
+				}
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Gets the maximum height among all charts. O(n) for each chart.
+		/// </summary>
+		public static float MaxValue(this FixedHorizontalSpacingCharts chartsGroup)
+		{
+			float value = float.NegativeInfinity;
+			unchecked {
+				foreach (var chart in chartsGroup.Charts) {
+					value = Math.Max(value, chart.MaxValue());
+				}
+			}
+			return value;
+		}
+	}
+}
